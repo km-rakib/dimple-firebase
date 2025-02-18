4859999b3dd9ed237fb3b0755d6285d052c18a02
Kazi Md. Rakibul Hassan <kazimdrakibulhassan@gmail.com>
2025-02-18 21:20:40 +0600

    google sign in completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 import './App.css'
+import { auth } from './firebase/firebase.init'
+import { useState } from 'react'
+
 
 function App() {
+  const [user, setUser] = useState(null)
+  const googleProvider = new GoogleAuthProvider()
   const handleGoogleSignIn = () =>{
-
+    signInWithPopup(auth, googleProvider)
+    .then((result) => {
+      console.log(result.user)
+      setUser(result.user)
+    })
+    .catch((error) =>{
+      console.log("ERROR", error)
+    })
   }
 
   return (
@@ -10,6 +23,13 @@ function App() {
       
       <h1>Dimple Firebase</h1>
       <button onClick={handleGoogleSignIn} className='btn btn-success'>Login with Google</button>
+      {
+        user && 
+        <div>
+          <h3> Name: {user.displayName} </h3>
+          <h4>Email: {user.email} </h4>
+        </div>
+      }
       
     </>
   )
